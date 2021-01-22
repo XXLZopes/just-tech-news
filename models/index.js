bb28a7d3e2f5e6cf5216dbc0d9cc4106245d91e1
XXLZopes <afordm99@gmail.com>
2021-01-21 16:54:06 -0800

    added CRUD routes to post modle

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post')
 
+//create associations
+
+//User can belong to many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//Post can belong to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
